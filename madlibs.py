40417c9264233f56fbad242055677045659bb9d5
jawish-a@outlook.com <jawish-a@outlook.com>
2020-08-16 11:40:13 +0300

    changed the variable names to match the practice

--- madlibs.py
+++ madlibs.py
@@ -1,26 +1,26 @@
 def main():
 	# write your code here
-	number = 1
-	plural_noun = ''
+	time = 1
+	items = ''
 	name = ''
-	sentence = ''
-	verb = ''
+	scream = ''
+	action = ''
 
 	print("Mad libs where libs get mad.")
 	print("Start Below:")
 	print("")
-	number = input("Enter a number from 1 to 12: ")
-	plural_noun = input("Enter a noun (plural): ")
+	time = input("Enter a number from 1 to 12: ")
+	items = input("Enter a noun (plural): ")
 	name = input("Enter a name: ")
-	sentence = input("Enter any sentence: ")
-	verb = input("Enter a verb: ")
+	scream = input("Enter any sentence: ")
+	action = input("Enter a verb: ")
 	print("")
 	print("The story goes")
 	print("")
-	print("It was " + str(number) + " O'clock when I heard a knock on the door.")
-	print("I opened the door and there was a box full of " + plural_noun + " with a note saying \"From Mr. " + name + ".\"")
-	print("Just as I closed the door I heard a scream \"" + sentence.upper() + ".\"")
-	print("I froze in place and all I could do was " + verb + ".")
+	print("It was " + str(time) + " O'clock when I heard a knock on the door.")
+	print("I opened the door and there was a box full of " + items + " with a note saying \"From Mr. " + name + ".\"")
+	print("Just as I closed the door I heard a scream \"" + scream.upper() + ".\"")
+	print("I froze in place and all I could do was " + action + ".")
 
 
 
