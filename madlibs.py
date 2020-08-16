a2e9b07f3648ecddd4c2756d114f0102254c2141
jawish-a@outlook.com <jawish-a@outlook.com>
2020-08-16 13:12:27 +0300

    capitalized the name string

--- madlibs.py
+++ madlibs.py
@@ -18,7 +18,7 @@ def main():
 	print("The story goes...")
 	print("")
 	print("It was " + str(time) + " o'clock when I heard a knock at the door.")
-	print("I opened the door and there was a box full of " + items + " with a note saying \"From Mr. " + name + ".\"")
+	print("I opened the door and there was a box full of " + items + " with a note saying \"From Mr. " + name.capitalize() + ".\"")
 	print("Just as I closed the door I heard a scream \"" + scream.upper() + ".\"")
 	print("I froze in place and all I could do was " + action + ".")
 
